61f88c4e5cf73426d8e15e6e9e2dce44bb19bf90
angelgarzz <angelgarzon42@gmail.com>
2021-08-19 19:12:18 -0400

    WalletConnect button styles

--- src/components/WalletConnect.tsx
+++ src/components/WalletConnect.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
-import { Dropdown, Menu } from 'antd';
+import { Dropdown, Menu, Button } from 'antd';
 import styled from 'styled-components';
 import { useWallet } from '../utils/wallet';
 import LinkAddress from './LinkAddress';
 
-const Button = styled(Dropdown.Button)`
+const DropdownButton = styled(Dropdown.Button)`
   & button {
     height: 40px;
     border: none;
     background-color: rgba(0, 253, 187, 0.2);
     color: #00fdbb;
   }
+
+  button:hover,
+  button:active,
+  button:focus {
+    color: #00fdbb;
+    background-color: rgba(0, 253, 187, 0.2);
+  }
+`;
+
+const ConnectButton = styled(Button)`
+  background-color: rgba(0, 253, 187, 0.2);
+  border-width: 0px;
+  color: #00fdbb;
+  font-size: 16px;
+  padding: 0 24px;
+
+  &:hover,
+  &:active,
+  &:focus {
+    background-color: rgba(0, 253, 187, 0.2);
+    color: #00fdbb;
+  }
 `;
 
 export default function WalletConnect() {
-  const { connected, wallet, select, connect, disconnect } = useWallet();
+  const { connected, wallet, select, disconnect } = useWallet();
   const publicKey = (connected && wallet?.publicKey?.toBase58()) || '';
 
   const menu = (
@@ -27,8 +49,16 @@ export default function WalletConnect() {
   );
 
   return (
-    <Button onClick={connected ? disconnect : connect} overlay={menu}>
-      {connected ? 'Disconnect' : 'Connect'}
-    </Button>
+    <>
+      {connected ? (
+        <DropdownButton onClick={disconnect} overlay={menu}>
+          Disconnect
+        </DropdownButton>
+      ) : (
+        <ConnectButton size="large" onClick={select}>
+          Connect wallet
+        </ConnectButton>
+      )}
+    </>
   );
 }
