bf3cd0364712b43f5a94f5db97edde0fa19856be
angelgarzz <angelgarzon42@gmail.com>
2021-08-19 01:23:10 -0400

    removed typography from UserInfoTable

--- src/components/UserInfoTable/index.jsx
+++ src/components/UserInfoTable/index.jsx
@@ -1,29 +1,18 @@
 import BalancesTable from './BalancesTable';
 import OpenOrderTable from './OpenOrderTable';
 import React from 'react';
-import { Tabs, Typography } from 'antd';
+import { Tabs } from 'antd';
 import FillsTable from './FillsTable';
 import FloatingElement from '../layout/FloatingElement';
 import FeesTable from './FeesTable';
 import { useOpenOrders, useBalances, useMarket } from '../../utils/markets';
 
-const { Paragraph } = Typography;
 const { TabPane } = Tabs;
 
 export default function Index() {
   const { market } = useMarket();
   return (
     <FloatingElement style={{ flex: 1, paddingTop: 20 }}>
-      <Typography>
-        <Paragraph style={{ color: 'rgba(255,255,255,0.5)' }}>
-          Make sure to go to Balances and click Settle to send out your funds.
-        </Paragraph>
-        <Paragraph style={{ color: 'rgba(255,255,255,0.5)' }}>
-          To fund your wallet, <a href="https://www.sollet.io">sollet.io</a>.
-          You can get SOL from FTX, Binance, BitMax, and others. You can get
-          other tokens from FTX.{' '}
-        </Paragraph>
-      </Typography>
       <Tabs defaultActiveKey="orders">
         <TabPane tab="Open Orders" key="orders">
           <OpenOrdersTab />
